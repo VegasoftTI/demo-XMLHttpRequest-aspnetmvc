dc5cbd155e5ceab6bc27c2b3e04ff26f6186a15b
Fabiano Nalin <fnalin@msn.com>
2014-11-21 23:58:33 -0200

    Subindo o projeto

---
+++ src/DemoXMLHttpRequest/DemoXMLHttpRequest/Controllers/DadosController.cs
@@ -0,0 +1,44 @@
+﻿using DemoXMLHttpRequest.Models;
+using System.Linq;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace DemoXMLHttpRequest.Controllers
+{
+    public class DadosController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CliJson()
+        {
+            System.Threading.Thread.Sleep(3000);
+            return Json(Clientes.ListaDeClientes, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult CliXML()
+        {
+            var dados = Clientes.ListaDeClientes;
+
+            //var xmlLINQ = new XElement("Clientes",
+            //    from cli in dados
+            //    select new XElement("Cliente",
+            //                    new XAttribute("ID", cli.ID),
+            //                    new XElement("Nome", cli.Nome)
+            //               ));
+
+            var xmlLambda = new XElement("Clientes", dados.Select(cli => new XElement("Cliente",
+                                new XAttribute("ID", cli.ID),
+                                new XAttribute("Nome", cli.Nome)
+                                //new XElement("Nome", cli.Nome),
+                           )));
+            System.Threading.Thread.Sleep(3000);
+            return Content(xmlLambda.ToString(), "text/xml");
+        }
+
+
+    }
+}
---
+++ src/DemoXMLHttpRequest/DemoXMLHttpRequest/Models/Clientes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoXMLHttpRequest.Models
+{
+    public class Cliente
+    {
+        public Cliente(string nome)
+        {
+            this.ID = Guid.NewGuid().ToString();
+            this.Nome = nome;
+        }
+        public string ID { get; private set; }
+        public string Nome { get; private set; }
+    }
+
+    public class Clientes
+    {
+        public static IList<Cliente> ListaDeClientes
+        {
+            get
+            {
+                return new List<Cliente> { 
+                new Cliente("Fabiano"),
+                new Cliente("Priscila"),
+                new Cliente("Raphael"),
+                new Cliente("Adriano"),
+                new Cliente("Daniel"),
+                new Cliente("Fernanda"),
+                new Cliente("Rebeca"),
+                new Cliente("Murilo"),
+                new Cliente("Vinícius"),
+                new Cliente("Isabel")
+            };
+            }
+        }
+    }
+}
